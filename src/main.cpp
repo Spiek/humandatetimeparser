2dd4b93a0e605444b987c308e02ea4c84673426c
Spiek <spiek@ymail.com>
2017-07-23 14:40:28 +0200

    improve example

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,23 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+	// init human date time parser
+    HumanDateTimeParser::parse("next day");
+
     QElapsedTimer timer;
     timer.start();
 
-
-    for(QString& element : QList<QString>{{"now"},{"10 September 2000"},{"+1 day"},{"+1 week"},{"+1 week 2 days 4 hours 2 seconds"},{"next Thursday"},{"last Monday"}, {"tomorrow"}})
+    qint64 elaspedSum = 0;
+    for(QString& element : QList<QString>{{"next day"}, {"now"},{"10 September 2000"},{"+1 day"},{"+1 week"},{"+1 week +2 days +4 hours +2 seconds"},{"next Thursday"},{"last Monday"}, {"tomorrow"}})
     {
         QElapsedTimer timersub;
         timersub.start();
         QDateTime time = HumanDateTimeParser::parse(element);
-        qDebug() << timersub.nsecsElapsed() << element << time;
+        qint64 elasped = timersub.nsecsElapsed();
+        elaspedSum += elasped;
+        qDebug("%7lld %s %s", timersub.nsecsElapsed(), qPrintable(time.toString("yyyy-MM-dd hh:mm:ss")), qPrintable(element));
     }
-    qDebug() << "Elapsed:" << timer.nsecsElapsed();
+    qDebug("%7lld Elapsed", elaspedSum);
 
 
     return a.exec();
