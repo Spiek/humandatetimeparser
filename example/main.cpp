62cf318741994464999fc40a4592e22502016250
Spiek <spiek@ymail.com>
2017-07-23 19:22:04 +0200

    code improving

--- example/main.cpp
+++ example/main.cpp
@@ -2,7 +2,9 @@
 #include <QDebug>
 #include "humandatetimeparser.h"
 
-#define PrettyPrint(a,datetime) qDebug("%s %s", qPrintable(HumanDateTimeParser::parse(a, currentDateTime).toString("yyyy-MM-dd hh:mm:ss")), a)
+#define PrettyPrint(a,datetime) \
+    qDebug("%s %s", \
+            qPrintable(HumanDateTimeParser::parse(a, currentDateTime).toString("yyyy-MM-dd hh:mm:ss")), a)
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +13,7 @@ int main(int argc, char *argv[])
     // get current time
     QDateTime currentDateTime = QDateTime::currentDateTime();
 
+    // day related
     PrettyPrint("now", currentDateTime);
     PrettyPrint("yesterday", currentDateTime);
     PrettyPrint("tomorrow", currentDateTime);
@@ -23,19 +26,19 @@ int main(int argc, char *argv[])
     PrettyPrint("next saturday", currentDateTime);
     PrettyPrint("last Thursday", currentDateTime);
 
-    // relative
+    // relative dates/times
     PrettyPrint("day", currentDateTime);
     PrettyPrint("1 year", currentDateTime);
     PrettyPrint("+1 month", currentDateTime);
     PrettyPrint("-1 day", currentDateTime);
     PrettyPrint("next year", currentDateTime);
 
-    // fix
+    // fix dates/times
     PrettyPrint("10 September 2000", currentDateTime);
     PrettyPrint("10.05.2017", currentDateTime);
     PrettyPrint("2017-05-10 14:04", currentDateTime);
 
-    // chain
+    // multiple commands in a chain
     PrettyPrint("2017-05-10 14:04 +2 days +2 seconds 12:04", currentDateTime);
     PrettyPrint("1 day +1 week +2 days +4 hours +2 seconds", currentDateTime);
 
